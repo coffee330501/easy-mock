59eaf1f2dec43a6cdfd0a51a2131f5e4b91ef4be
objcfeng <1034440274@qq.com>
2024-01-09 09:47:29 +0800

    feat: 删除测试代码

--- src/main/java/io/github/coffee330501/easyMock/MockStrategies.java
+++ src/main/java/io/github/coffee330501/easyMock/MockStrategies.java
@@ -14,9 +14,4 @@ public class MockStrategies {
     static MockStrategy<Integer> INTEGER_MOCK = param -> ThreadLocalRandom.current().nextInt(0, 999999);
     static MockStrategy<Long> LONG_MOCK = param -> ThreadLocalRandom.current().nextLong(0L, 9999999999L);
     static MockStrategy<BigDecimal> BIG_DECIMAL_MOCK = param -> new BigDecimal(String.valueOf(Math.random() * 10000)).setScale(2, RoundingMode.DOWN);
-
-    public static void main(String[] args) {
-        System.out.println(LONG_MOCK.mock());
-    }
-
 }
